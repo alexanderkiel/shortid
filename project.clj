9c3007af9b0950fea863dd6a4852c8dbf9349054
Alexander Kiel <alexanderkiel@gmx.net>
2015-05-05 10:31:43 +0200

    Bump Version to 0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akiel/shortid "0.1-SNAPSHOT"
+(defproject org.clojars.akiel/shortid "0.1"
   :description "Generates short base62 encoded identifiers."
   :url "https://github.com/alexanderkiel/shortid"
   :license {:name "Eclipse Public License"
