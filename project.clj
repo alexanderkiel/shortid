04c3cb376e07783dbed599d4497f31bcaa66440a
Alexander Kiel <alexanderkiel@gmx.net>
2015-05-05 10:30:57 +0200

    Use Clojars GroupId

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject shortid "0.1-SNAPSHOT"
+(defproject org.clojars.akiel/shortid "0.1-SNAPSHOT"
   :description "Generates short base62 encoded identifiers."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/alexanderkiel/shortid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
